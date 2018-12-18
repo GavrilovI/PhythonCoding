42d706b458fee98e143aeab3c12a7e20287d2ba3
GavrilovI <45885708+GavrilovI@users.noreply.github.com>
2018-12-18 11:39:17 +0000

    Add files via upload

---
+++ Coding.py
@@ -0,0 +1,14 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a=17
+>>> b=56
+>>> dif=b-a
+>>> print ("diference betwen",b,"and",a,"is",dif)
+diference betwen 56 and 17 is 39
+>>> div=b/a
+>>> print ("divide betwen",b, "and",a,"is",div)
+divide betwen 56 and 17 is 3.2941176470588234
+>>> mul=a*b
+>>> print ("multiple betwen",a,"and",b,"is",mul)
+multiple betwen 17 and 56 is 952
+>>> 
