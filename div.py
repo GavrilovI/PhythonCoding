0a0d5c18ab9b48ceb36a2e20fad867e32054cdf4
GavrilovI <45885708+GavrilovI@users.noreply.github.com>
2018-12-18 11:20:45 +0000

    Add files via upload

---
+++ div.py
@@ -0,0 +1,26 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x=20
+>>> a=15
+>>> dif=x-a
+>>> print ("diference between " ,x, "and" ,a, "is:", dif)
+diference between  20 and 15 is: 5
+>>> print ("difference between", x,"and", a,"is:",dif)
+difference between 20 and 15 is: 5
+>>> 
+>>> d=10
+>>> b=2
+>>> div=10/2
+>>> print ("divide betwen " ,d, "and" ,b, "is:", div)
+divide betwen  10 and 2 is: 5.0
+>>> c=5
+>>> e=5
+>>> mul=5*5
+>>> print ("multiple between" ,c, "and" ,e, "is:" mul)
+SyntaxError: invalid syntax
+>>> print ("multiple between" ,c, "and" ,e, "is:" mult)
+SyntaxError: invalid syntax
+>>> 
+>>> 
+>>> 
+>>> 
